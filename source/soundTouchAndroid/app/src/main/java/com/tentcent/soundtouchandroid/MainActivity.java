aac82d6b39d2b7ac21d3e89aa2150852e77bcb22
Zhengbledor <gazhjky@qq.com>
2023-02-16 16:29:43 +0800

    Generate .so files

--- source/soundTouchAndroid/app/src/main/java/com/tentcent/soundtouchandroid/MainActivity.java
+++ source/soundTouchAndroid/app/src/main/java/com/tentcent/soundtouchandroid/MainActivity.java
@@ -14,23 +14,9 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("soundtouchandroid");
     }
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
     }
-
-    /**
-     * A native method that is implemented by the 'soundtouchandroid' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }

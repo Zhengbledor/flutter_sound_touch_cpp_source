4c58abf8d577b63c3b8d8511443e6edc0aa2ca58
Zhengbledor <gazhjky@qq.com>
2023-02-17 16:28:19 +0800

    Generate .so files

---
+++ soundtouchios/soundtouchios/soundtouchios.h
@@ -0,0 +1,12 @@
+//
+//  soundtouchios.h
+//  soundtouchios
+//
+//  Created by 郑志豪 on 2023/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface soundtouchios : NSObject
+
+@end
---
+++ soundtouchios/soundtouchios/soundtouchios.m
@@ -0,0 +1,12 @@
+//
+//  soundtouchios.m
+//  soundtouchios
+//
+//  Created by 郑志豪 on 2023/2/17.
+//
+
+#import "soundtouchios.h"
+
+@implementation soundtouchios
+
+@end
